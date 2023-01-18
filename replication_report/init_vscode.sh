f6283491e87871f39b96c8817ae2afe8e144f7d9
fBedecarrats <f.bedecarrats@gmail.com>
2023-01-18 23:38:17 +0100

    an init script for vs code (with R install)

---
+++ replication_report/init_vscode.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Install Google CLI
+# sudo apt-get update
+# sudo apt-get install apt-transport-https ca-certificates gnupg
+# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# sudo apt-get update && sudo apt-get install google-cloud-cli
+
+# Install R (from https://rtask.thinkr.fr/installation-of-r-4-2-on-ubuntu-22-04-lts-and-tips-for-spatial-packages/)
+sudo apt install -y --no-install-recommends software-properties-common dirmngr
+# Add the keys
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+# Create variables
+WORK_DIR=/home/onyxia/work/PA_matching
+REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/fBedecarrats/PA_matching.git # As initial
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R onyxia:users $WORK_DIR
+
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && !identical(getwd(), \"'$WORK_DIR'\"))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('$WORK_DIR')
+            # For a slick dark theme
+            rstudioapi::applyTheme('Merbivore')
+            # Console where it should be
+            rstudioapi::executeCommand('layoutConsoleOnRight')
+            }
+            }, action = 'append')
+            " >> /home/onyxia/work/.Rprofile
