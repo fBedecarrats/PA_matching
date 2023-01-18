331300cce417dca4068578fb6d1641b5ea3ed5b1
fBedecarrats <f.bedecarrats@gmail.com>
2023-01-18 23:40:25 +0100

    Removing R specific parts of init script

--- replication_report/init_vscode.sh
+++ replication_report/init_vscode.sh
@@ -21,20 +21,3 @@ REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/fBedecarrats/PA_matchin
 # Git
 git clone $REPO_URL $WORK_DIR
 chown -R onyxia:users $WORK_DIR
-
-# launch RStudio in the right project
-# Copied from InseeLab UtilitR
-    echo \
-    "
-    setHook('rstudio.sessionInit', function(newSession) {
-        if (newSession && !identical(getwd(), \"'$WORK_DIR'\"))
-        {
-            message('On charge directement le bon projet :-) ')
-            rstudioapi::openProject('$WORK_DIR')
-            # For a slick dark theme
-            rstudioapi::applyTheme('Merbivore')
-            # Console where it should be
-            rstudioapi::executeCommand('layoutConsoleOnRight')
-            }
-            }, action = 'append')
-            " >> /home/onyxia/work/.Rprofile
