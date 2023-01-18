b5b177cf1a10cf6ccba4794fdf7a6fda737c5766
fBedecarrats <f.bedecarrats@gmail.com>
2023-01-19 00:08:56 +0100

    add apt updates

--- replication_report/init_vscode.sh
+++ replication_report/init_vscode.sh
@@ -6,12 +6,14 @@
 # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 # sudo apt-get update && sudo apt-get install google-cloud-cli
 
+sudo apt update
 # Install R (from https://rtask.thinkr.fr/installation-of-r-4-2-on-ubuntu-22-04-lts-and-tips-for-spatial-packages/)
 sudo apt install -y --no-install-recommends software-properties-common dirmngr
 # Add the keys
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo apt update
 # Install R
 sudo apt install -y r-base r-base-core r-recommended r-base-dev
 
