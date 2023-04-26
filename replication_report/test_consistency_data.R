08bc9d349c2f9110cde8bf317c39313ae6f21343
fBedecarrats <f.bedecarrats@gmail.com>
2023-04-26 08:04:44 +0000

    Converts Wolf data to tabular

--- replication_report/test_consistency_data.R
+++ replication_report/test_consistency_data.R
@@ -40,26 +40,156 @@ if (!dir.exists("PA_matching_asis/processed_rasters")) {
   map2(my_tifs, my_tifs_dest, save_from_s3)
 }
 
+if (!file.exists("PA_matching_asis/aois.gpkg")) {
+  aws.s3::save_object(
+    object = "Replication_wolf/aois.gpkg",
+    bucket = "fbedecarrats",
+    file = "PA_matching_asis/aois.gpkg",
+    overwrite = TRUE,
+    region = "")
+}
+
+
+
 # Load files into memory
 files <- list.files("PA_matching_asis/processed_rasters", pattern = "\\.tif$", 
                     full.names = TRUE) # List available files
 file_names <- list.files("PA_matching_asis/processed_rasters", pattern = "\\.tif$", 
                          full.names = FALSE) %>%
   str_remove("\\.tif") # Prepare the names (no path or extension)
-data <- map(files, rast) # Load in a list
+data <- map2(files, file_names, ~ rast(.x, names = )) # Load in a list
 names(data) <- file_names # name the elements
 list2env(data, envir = .GlobalEnv) # split the elements
+rm(data)
 data <- c(carbon, countries, cover, cover_loss, drivers, ecoregions,
           elev, forest_biome, gain, loss, lossyear, non_threatened, PAs,
           PAs_buffer, pop_dens, slope, threatened, travel_time) 
+names(data) <- file_names
 
-aois <- read_sf("revamp/aois/aois.gpkg")
+# Prepare AOIs to iterate upon
+aois <- read_sf("PA_matching_asis/aois.gpkg") %>%
+  st_transform(crs = crs(cover))
 
-data %>%
-  crop()
-  filter(cover > 0) %>%
-  as.data.frame() %>%
-  write_parquet("PA_matching_asis/processed_rasters/consolidated.parquet")
+all_data <- data.frame()
+n_aois <- nrow(aois)
+for (i in 1:nrow(aois)) {
+  aoi <- aois[i, ]
+  print(paste(aoi$GID_0, i, "/", n_aois))
+  aoi_data <- data %>%
+    crop(aoi) %>%
+    mask(aoi) %>%
+    as.data.frame() %>%
+    filter(ecoregions >= 0 & countries >= 0 & drivers >= 0 & forest_biome == 1 &
+             slope >= 0 & !is.na(cover_loss) & !is.na(pop_dens))
+  all_data <- bind_rows(all_data, aoi_data)
+  rm(aoi_data)
+}
+write_parquet(all_data, "PA_matching_asis/processed_rasters/all_data.parquet")
+aws.s3::put_object(file = "PA_matching_asis/processed_rasters/all_data.parquet",
+                   object = "PA_matching_asis/all_data.parquet",
+                   bucket = "fbedecarrats",
+                   region = "",
+                   overwrite = TRUE,
+                   multipart = TRUE)
+
+data <- read_parquet("PA_matching_asis/processed_rasters/all_data.parquet",
+                     as_data_frame = FALSE)
+stats <- data %>%
+  mutate(with_forest_cover = cover > 0,
+         with_loss = loss > 0,
+         with_lossyear = lossyear > 0) %>%
+  group_by(with_forest_cover, with_loss, with_lossyear) %>%
+  summarise(n = n()) %>%
+  mutate(percent = round(n / sum(stats$n) * 100, 2)) %>%
+  collect()
+
+stats2 <- data %>%
+  mutate(with_forest_cover = cover > 0,
+         with_loss = loss > 0,
+         with_lossyear = lossyear > 0,
+         is_treatment = case_when(
+           PAs == 1 ~ "PA",
+           PAs_buffer == 1 ~ "PA_buffer",
+           .default = "Control")) %>%
+  group_by(with_forest_cover, with_loss, with_lossyear, is_treatment) %>%
+  summarise(n = n()) %>%
+  collect()
+
+stats3 <- data %>%
+  mutate(with_loss = loss > 0,
+         with_lossyear = lossyear > 0) %>%
+  group_by(with_loss, with_lossyear) %>%
+  summarise(n = n()) %>%
+  collect()
+stats3 <- stats3 %>%
+  mutate(percent = round(n / sum(stats3$n) * 100, 2))
+
+library(haven)
+read_parquet("PA_matching_asis/processed_rasters/all_data.parquet",
+             as_data_frame = TRUE) %>%
+  write_dta("data_as_wolf.dta")
+
+
+stats2_bal <- stats2 %>%
+  filter(with_forest_cover & with_loss)
+  
+
+data_mdg %>%
+  summarise(n = n(),
+            with_cover = sum(cover > 0),
+            loss_noyear = sum((loss > 0 & lossyear == 0)),
+            loss_noyear_share = loss_noyear / n * 100,
+            loss_noyear_share_cover = loss_noyear / with_cover * 100)
+
+countries %>%
+  crop(aoi_mada) %>%
+  plot()
+
+hist(test$lossyear)
+
+cover2 <- rast("PA_matching_asis/processed_rasters/cover.tif", lyrs = c("cover"))
+
+carbon2 <- rast("PA_matching_asis/processed_rasters/carbon.tif", lyrs = 1, names = c("carbon"))
+identical(cover, cover2)
+
+
+
+
+
+aoi_mada2 <- st_transform(aoi_mada, crs = crs(cover2))
+
+cover3 <- cover2 %>%
+  crop(aoi_mada2)
+
+plot(cover3)
+
+tm_shape(cover2) +
+  tm_raster()
+
+# aoi <- aois %>%
+#   mutate(rownum = row_number(), .before = everything()) %>%
+#   filter(GID_0 == "MDG")
+aois_mada <- aois %>%
+  filter(GID_0 == "MDG" & biome_num == 1)
+
+lossyear <- rast("PA_matching_asis/processed_rasters/lossyear.tif") %>%
+  crop(aois_mada)
+loss <- rast("PA_matching_asis/processed_rasters/loss.tif") %>%
+  crop(aois_mada)
+both <- c(lossyear, loss)
+
+
+tm_shape(loss) +
+  tm_raster() + 
+  tm_shape(aois_mada) + 
+  tm_polygons()
+
+cover_mdg <- cover %>%
+  crop(aoi) %>%
+  as.data.frame()
+
+cover_mdg2 <- cover_mdg %>%
+  filter(!is.na(cover))
 
 all_df <- read_parquet("PA_matching_asis/processed_rasters/consolidated.parquet")
 test <- all_df %>%
