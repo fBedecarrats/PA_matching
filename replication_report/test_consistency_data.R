6e147f8efd9d6315d2601fa62075d8cc624086cf
fBedecarrats <f.bedecarrats@gmail.com>
2023-04-29 11:16:07 +0200

    Test PAs format in source raster

--- replication_report/test_consistency_data.R
+++ replication_report/test_consistency_data.R
@@ -12,6 +12,15 @@ library(MatchIt)
 library(tictoc)
 setwd("replication_report")
 
+# # Teste format de la variable PAs
+# pas <- rast("PA_matching_asis/processed_rasters/PAs.tif")
+# pas_df <- pas %>%
+#   as.data.frame()
+# test <- pas_df %>%
+#   group_by(layer) %>%
+#   summarise(n = n())
+# ###############
+
 if (!dir.exists("PA_matching_asis/processed_rasters")) {
   dir.create("PA_matching_asis")
   dir.create("PA_matching_asis/processed_rasters")
@@ -92,8 +101,19 @@ aws.s3::put_object(file = "PA_matching_asis/processed_rasters/all_data.parquet",
                    overwrite = TRUE,
                    multipart = TRUE)
 
-data <- read_parquet("PA_matching_asis/processed_rasters/all_data.parquet",
-                     as_data_frame = FALSE)
+# data <- read_parquet("PA_matching_asis/processed_rasters/all_data.parquet",
+#                      as_data_frame = FALSE)
+data <- read_parquet("PA_matching_asis/processed_rasters/all_data.parquet")
+
+test2 <- data %>%
+  group_by(PAs) %>%
+  summarise(n = n())
+
+data %>%
+  summarise(sum(PAs > 0),
+            sum(PAs > 1),
+            sum(PAs > 1000))
+
 # Stats for incomplete data 
 stats <- data %>%
   mutate(with_forest_cover = cover > 0,
@@ -104,12 +124,13 @@ stats <- data %>%
   collect()
 stats <- stats %>%
   mutate(percent = round(n / sum(stats$n) * 100, 2)) 
+
 # By treatmnent/control
 stats2 <- data %>%
   mutate(with_forest_cover = cover > 0,
          with_loss = loss > 0,
          with_lossyear = lossyear > 0,
-         is_treatment = PAs == 1) %>%
+         is_treatment = PAs > 0) %>%
   group_by(with_forest_cover, with_loss, with_lossyear, is_treatment) %>%
   summarise(n = n()) %>%
   collect() %>%
@@ -122,6 +143,171 @@ no_lossyear_control = (stats2 %>%
   (ungroup(stats2) %>% 
      filter(with_forest_cover, with_loss, !is_treatment) %>%
      summarise(n = sum(n)) %>% pluck("n")) 
+
+no_lossyear_treatment = (stats2 %>% 
+                         filter(with_forest_cover, with_loss, is_treatment) %>%
+                         filter(!with_lossyear) %>%
+                         pluck("n")) /
+  (ungroup(stats2) %>% 
+     filter(with_forest_cover, with_loss, is_treatment) %>%
+     summarise(n = sum(n)) %>% pluck("n"))
+
+
+
+# By treatmnent/control : FILTER COVER 30%
+stats3 <- data %>%
+  mutate(with_forest_cover = cover > 0.3,
+         with_loss = loss > 0,
+         with_lossyear = lossyear > 0,
+         is_treatment = PAs > 0) %>%
+  group_by(with_forest_cover, with_loss, with_lossyear, is_treatment) %>%
+  summarise(n = n()) %>%
+  collect() %>%
+  filter(with_forest_cover & with_loss) 
+
+no_lossyear_control_mincover = (stats3 %>% 
+                         filter(with_forest_cover, with_loss, !is_treatment) %>%
+                         filter(!with_lossyear) %>%
+                         pluck("n")) /
+  (ungroup(stats3) %>% 
+     filter(with_forest_cover, with_loss, !is_treatment) %>%
+     summarise(n = sum(n)) %>% pluck("n")) 
+
+no_lossyear_treatment_mincover = (stats3 %>% 
+                           filter(with_forest_cover, with_loss, is_treatment) %>%
+                           filter(!with_lossyear) %>%
+                           pluck("n")) /
+  (ungroup(stats2) %>% 
+     filter(with_forest_cover, with_loss, is_treatment) %>%
+     summarise(n = sum(n)) %>% pluck("n"))
+
+wdpa_ids <- data %>%
+  group_by(PAs) %>%
+  summarise(n = n()) %>%
+  collect()
+
+# Matching
+tic()
+matched_all <- matchit(# the first line of covar is coarsened
+  formula = PAs ~ elev + slope + cover + travel_time + pop_dens +
+    countries + ecoregions + drivers, # these on 2nd line remain exact
+  method = "cem",
+  # for cutpoints 0 means no coarsening, and q5 means quintiles
+  cutpoints = list(elev = "q5", slope = "q5", cover = "q5",
+                   travel_time = "q5", pop_dens = "q5",
+                   countries = 0, ecoregions = 0, drivers = 0),
+  # k2k = TRUE, # To get a 1-to-1 matching 
+  data = collect(data))
+toc() # 408.53 sec elapsed
+
+tic()
+matched_data <- match.data(matched_all)
+toc() # 120.36 sec elapsed
+
+matched_data <-  matched_data %>%
+  mutate(subclass = as.integer(subclass))
+
+dir.create("PA_matching_asis/consolidated")
+write_parquet(matched_data, "PA_matching_asis/consolidated/matched_data.parquet")
+# matched_data <- read_parquet("revamp/consolidated/matched_data.parquet")
+# tic()
+matched_data %>%
+  group_by(PAs) %>%
+  write_dataset("PA_matching_asis/consolidated/matched_data")
+rm(list = c("matched_data", "matched_all", "data"))
+
+# A function that computes the deforestation before and after status year for
+# protected area pixels, and before and after matched treatment status year for 
+# control pixels. Uses arrow for fast and larger-than-memory calculations
+calc_defor_mode <- function(first_year, last_year, matched_treatment,
+                            matched_control) {
+  subset_trt <- matched_treatment %>%
+    filter(status_year %in% first_year:last_year)
+  
+  subset_ctrl <- matched_control %>%
+    select(-status_year)
+  
+  subclasses_trt <- subset_trt %>%
+    select(subclass, status_year) %>%
+    unique()
+  
+  defor_to_compute <- subclasses_trt %>%
+    inner_join(subset_ctrl, by = "subclass")
+  
+  defor_computed <- defor_to_compute %>%
+    mutate(before = lossyear >= first_year & lossyear < status_year,
+           before = as.integer(before), 
+           after = lossyear > status_year & lossyear <= last_year,
+           after = as.integer(after),
+           CL_b = cover * loss * before,
+           CL_a = cover * loss * after) %>%
+    group_by(subclass, status_year) %>%
+    summarise(C = sum(cover, na.rm = TRUE),
+              CL = sum(cover * loss, na.rm = TRUE),
+              CL_b = sum(CL_b, na.rm = TRUE),
+              CL_a = sum(CL_a, na.rm = TRUE)) %>%
+    mutate(Y_b = status_year - first_year,
+           Y_a = last_year - status_year,
+           loss = - (((C - CL)/C)^(1/18) - 1),
+           loss_before = - (((C - CL_b)/C)^(1/Y_b) - 1),
+           loss_after = - (((C - CL_a)/C)^(1/Y_a) - 1)) %>%
+    select(subclass, status_year, ctrl_loss = loss, 
+           ctrl_loss_before = loss_before, ctrl_loss_after = loss_after)
+  
+  expanded <- subset_trt %>%
+    left_join(defor_computed, by = c("subclass", "status_year"))
+  
+  complete <- expanded %>%
+    ungroup() %>%
+    mutate(before = lossyear >= first_year & lossyear < status_year,
+           before = as.integer(before), 
+           after = lossyear > status_year & lossyear <= last_year,
+           after = as.integer(after),
+           C = cover,
+           CL = cover * loss,
+           CL_b = CL * before,
+           CL_a = CL * after,
+           Y_b = status_year - first_year,
+           Y_a = last_year - status_year,
+           loss = - (((C - CL)/C)^(1/18) - 1),
+           loss_before = - (((C - CL_b)/C)^(1/Y_b) - 1),
+           loss_after = - (((C - CL_a)/C)^(1/Y_a) - 1)) %>%
+    select(-before, -after, -C, -CL, -CL_b, -CL_a, -Y_a, -Y_b) %>%
+    rename(trt_loss = loss, 
+           trt_loss_before = loss_before, 
+           trt_loss_after = loss_after)
+  return(collect(complete))
+}
+
+# Set parameters as in the original study
+tic()
+df <- calc_defor_mode(
+  first_year = 2001,
+  last_year = 2018,
+  matched_treatment = open_dataset("PA_matching_asis/consolidated/matched_data/PAs=1"),
+  matched_control  = open_dataset("PA_matching_asis/consolidated/matched_data/PAs=0"))
+toc() # 280.07 sec elapsed
+
+# Write output
+tic()
+write_parquet(df, "revamp/consolidated/matched_df.parquet")
+toc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 no_lossyear_treatment = (stats2 %>% 
