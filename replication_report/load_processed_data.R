839952e35b1d8ca6c2a6f8f6ec5756896835a72b
fBedecarrats <f.bedecarrats@gmail.com>
2023-05-13 09:41:26 +0200

    Load preprocessed data

---
+++ replication_report/load_processed_data.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(aws.s3)
+
+if (!stringr::str_ends(getwd(), "replication_report")) {
+  setwd("replication_report")
+}
+
+
+# A function to put data from local machine to S3
+put_to_s3 <- function(from, to) {
+  aws.s3::put_object(
+    file = from,
+    object = to,
+    bucket = "fbedecarrats",
+    region = "",
+    multipart = TRUE)
+}
+
+# A function to iterate/vectorize copy
+save_from_s3 <- function(from, to) {
+  aws.s3::save_object(
+    object = from,
+    bucket = "fbedecarrats",
+    file = to,
+    overwrite = FALSE,
+    region = "")
+}
+
+# Listing files in bucket
+my_files <- get_bucket_df(bucket = "fbedecarrats",
+                          prefix = "data_processed",
+                          region = "") %>%
+  pluck("Key")
+
+map2(my_files, my_files, save_from_s3)
